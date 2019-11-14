77e85d10c2f4b40920a6d8ab36796fa812cabb6f
Milan <milan.dhar@gmail.com>
2019-11-13 19:02:38 -0800

    added droppable

--- src/components/StarredProjectsList.js
+++ src/components/StarredProjectsList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Button, Icon, Table, Flag } from 'semantic-ui-react'
 import { withRouter } from 'react-router-dom'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 
 class StarredProjectsList extends Component {
 
@@ -14,9 +15,14 @@ class StarredProjectsList extends Component {
     this.props.history.push("/donate")
   }
 
+  onDragEnd = (result) => {
+    // ToDo: Reorder our table
+  }
+
 
   render(){
     return(
+    <DragDropContext onDragEnd={this.onDragEnd}>
       <Table singleLine>
         <Table.Header>
           <Table.Row>
@@ -26,31 +32,42 @@ class StarredProjectsList extends Component {
             <Table.HeaderCell>Action</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
-        <Table.Body>
-          {this.props.projectArray.map((project, idx) => {
-            return (<Table.Row key={project.id} className="project">
-              <Table.Cell>
-                {project.country.name}
-                <Flag name={project.country.name.toLowerCase()}/>
-              </Table.Cell>
-              <Table.Cell width={5}>
-                {project.title}
-              </Table.Cell>
-              <Table.Cell className="fundingCell">
-              Funding: ${project.funding.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} / Goal: ${project.goal.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}
-              </Table.Cell>
-              <Table.Cell id="remove-donate-div">
-                <Button color='red' onClick={()=>this.handleRemove(project)} icon>
-                  <Icon name='close' />
-                </Button>
-                <Button color='green' onClick={()=>this.goToDonation(project)} icon>
-                  <Icon name='dollar sign' />
-                </Button>
-              </Table.Cell>
-            </Table.Row>)
-          })}
-        </Table.Body>
+        <Droppable droppableId="table">
+          {(droppableProvided: DroppableProvided) => (
+            <Table.Body
+              ref={(ref: ?HTMLElement) => {
+                this.tableRef = ref;
+                droppableProvided.innerRef(ref)
+              }}
+              {...droppableProvided.droppableProps}
+              >
+              {this.props.projectArray.map((project, idx) => {
+                return (<Table.Row key={project.id} className="project">
+                  <Table.Cell>
+                    {project.country.name}
+                    <Flag name={project.country.name.toLowerCase()}/>
+                  </Table.Cell>
+                  <Table.Cell width={5}>
+                    {project.title}
+                  </Table.Cell>
+                  <Table.Cell className="fundingCell">
+                  Funding: ${project.funding.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} / Goal: ${project.goal.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}
+                  </Table.Cell>
+                  <Table.Cell id="remove-donate-div">
+                    <Button color='red' onClick={()=>this.handleRemove(project)} icon>
+                      <Icon name='close' />
+                    </Button>
+                    <Button color='green' onClick={()=>this.goToDonation(project)} icon>
+                      <Icon name='dollar sign' />
+                    </Button>
+                  </Table.Cell>
+                </Table.Row>)
+              })}
+            </Table.Body>
+          )}
+        </Droppable>
       </Table>
+    </DragDropContext>
     )
   }
 }
