09275d6bbf0f981cf292afbaca215db5d91ac41b
Milan <milan.dhar@gmail.com>
2019-11-13 19:56:31 -0800

    starting the new semantic ui demo

--- src/components/StarredProjectsList.js
+++ src/components/StarredProjectsList.js
@@ -5,8 +5,16 @@ import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
 class StarredProjectsList extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      starredProjects: this.props.projectArray,
+      reorderEnabled: false,
+    }
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
+
   handleRemove = (project) => {
-    console.log(project)
     this.props.removeFavorite(project.id)
   }
 
@@ -17,71 +25,71 @@ class StarredProjectsList extends Component {
 
   onDragEnd = (result) => {
     // ToDo: Reorder our table
+
   }
 
 
   render(){
     return(
-    <DragDropContext onDragEnd={this.onDragEnd}>
-      <Table singleLine>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Country</Table.HeaderCell>
-            <Table.HeaderCell>Project Title</Table.HeaderCell>
-            <Table.HeaderCell>Funding / Goal</Table.HeaderCell>
-            <Table.HeaderCell>Action</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Droppable droppableId="table">
-          {(droppableProvided: DroppableProvided) => (
-            <Ref innerRef={DroppableProvided.innerRef}>
-              <Table.Body
-                ref={(ref: ?HTMLElement) => {
-                  this.tableRef = ref;
-                  droppableProvided.innerRef(ref)
-                }}
-                {...droppableProvided.droppableProps}
-                >
-                {this.props.projectArray.map((project, idx) => {
-                  return (
-                    <Draggable
-                      draggableId={project.id}
-                      index={idx}
-                      key={project.id}>
-                      {(
-                        provided: DraggableProvided,
-                        snapshot: DraggableStateSnapshot,
-                      ) => (
-                        <Table.Row key={project.id} className="project">
-                        <Table.Cell>
-                          {project.country.name}
-                          <Flag name={project.country.name.toLowerCase()}/>
-                        </Table.Cell>
-                        <Table.Cell width={5}>
-                          {project.title}
-                        </Table.Cell>
-                        <Table.Cell className="fundingCell">
-                        Funding: ${project.funding.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} / Goal: ${project.goal.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}
-                        </Table.Cell>
-                        <Table.Cell id="remove-donate-div">
-                          <Button color='red' onClick={()=>this.handleRemove(project)} icon>
-                            <Icon name='close' />
-                          </Button>
-                          <Button color='green' onClick={()=>this.goToDonation(project)} icon>
-                            <Icon name='dollar sign' />
-                          </Button>
-                        </Table.Cell>
-                      </Table.Row>
-                      )}
-                  </Draggable>)
-                })}
-                {droppableProvided.placeholder}
-              </Table.Body>
-            </Ref>
-          )}
-        </Droppable>
-      </Table>
-    </DragDropContext>
+    <div>
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Table singleLine>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Country</Table.HeaderCell>
+              <Table.HeaderCell>Project Title</Table.HeaderCell>
+              <Table.HeaderCell>Funding / Goal</Table.HeaderCell>
+              <Table.HeaderCell>Action</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Droppable droppableId="table">
+            {(provided, snapshot) => (
+              <Ref innerRef={provided.innerRef}>
+                <Table.Body {...provided.droppableProps}>
+                  {this.props.projectArray.map((project, idx) => {
+                    return (
+                      <Draggable
+                        draggableId={project.id}
+                        index={idx}
+                        key={project.id}
+                      >
+                        {(provided, snapshot) => (
+                        <Ref innerRef={provided.innerRef}>
+                          <Table.Row {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            key={project.id}
+                            className="project">
+                            <Table.Cell>
+                              {project.country.name}
+                              <Flag name={project.country.name.toLowerCase()}/>
+                            </Table.Cell>
+                            <Table.Cell width={5}>
+                              {project.title}
+                            </Table.Cell>
+                            <Table.Cell className="fundingCell">
+                            Funding: ${project.funding.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} / Goal: ${project.goal.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}
+                            </Table.Cell>
+                            <Table.Cell id="remove-donate-div">
+                              <Button color='red' onClick={()=>this.handleRemove(project)} icon>
+                                <Icon name='close' />
+                              </Button>
+                              <Button color='green' onClick={()=>this.goToDonation(project)} icon>
+                                <Icon name='dollar sign' />
+                              </Button>
+                            </Table.Cell>
+                          </Table.Row>
+                        </Ref>
+                        )}
+                    </Draggable>)
+                  })}
+                  {provided.placeholder}
+                </Table.Body>
+              </Ref>
+            )}
+          </Droppable>
+        </Table>
+      </DragDropContext>
+    </div>
     )
   }
 }
